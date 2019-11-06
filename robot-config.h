b4d493a1c5a562f36e0ddde88b40d596fd73a8e9
kathansheth04 <50056395+kathansheth04@users.noreply.github.com>
2019-11-05 23:25:35 -0800

    Update robot-config.h

--- robot-config.h
+++ robot-config.h
@@ -8,10 +8,10 @@
 //
 vex::brain Brain;
 vex::controller Controller = vex::controller();
-vex::motor LeftF = vex::motor(vex::PORT1);
-vex::motor RightF = vex::motor(vex::PORT2);
-vex::motor LeftB = vex::motor(vex::PORT3);
-vex::motor RightB = vex::motor(vex::PORT4);
-vex::motor lift = vex::motor(vex::PORT5);
-vex::motor intake = vex::motor(vex::PORT6);
-vex::motor intake2 = vex::motor(vex::PORT7);
+vex::motor LeftF = vex::motor(vex::PORT1); //left front motor connected to port1 of vex brain
+vex::motor RightF = vex::motor(vex::PORT2); //right front motor connected to port2
+vex::motor LeftB = vex::motor(vex::PORT3); // left back motor connected to port 3
+vex::motor RightB = vex::motor(vex::PORT4); //right back motor connected to port 4
+vex::motor lift = vex::motor(vex::PORT5); //lift motor with pid connected to port5
+vex::motor intake = vex::motor(vex::PORT6); //intake motor 1 connected to port 6
+vex::motor intake2 = vex::motor(vex::PORT7); //intake motor 2 connected to port 7.
