a9f52cc2d865e07a2e653f67cf3fdbbb691e77e0
kathansheth04 <50056395+kathansheth04@users.noreply.github.com>
2020-02-01 23:31:26 -0800

    Update vex.h

--- vex.h
+++ vex.h
@@ -1,12 +1,4 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       vex.h                                                     */
-/*    Author:       Vex Robotics                                              */
-/*    Created:      1 Feb 2019                                                */
-/*    Description:  Default header for V5 projects                            */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-//
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
